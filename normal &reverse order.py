56a6f800dafb4405147d9eba2959a01bd5d8e987
padmashree043 <padmashree917@gmail.com>
2024-09-27 17:42:29 +0530

    Add files via upload

---
+++ normal &reverse order.py
@@ -0,0 +1,5 @@
+word = "mosque"
+normal_order = ''.join(sorted(word))
+reverse_order = ''.join(sorted(word, reverse=True))
+print(f"Normal Order: {normal_order}")
+print(f"Reverse Order: {reverse_order}")
